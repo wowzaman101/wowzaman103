1302b5cb05859180ba3fe9809f2f1c59e69ba99d
TS <thanyawat.s@7solutions.co.th>
2025-07-18 14:53:52 +0700

    fix: add CORS middleware to allow all origins in server configuration

--- infrastructure/server/server.go
+++ infrastructure/server/server.go
@@ -4,11 +4,15 @@ import (
 	"coding-games/internal/handler/gamehdl"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func New(gh gamehdl.Handler) *fiber.App {
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+	}))
 	// Health check route
 	app.Get("/health", func(c fiber.Ctx) error {
 		return c.SendString("OK")
